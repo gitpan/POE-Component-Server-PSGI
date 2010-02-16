8c1fafb08e66ea3a8709198951c34a2975699fca
Paul Driver <frodwith@cpan.org>
2010-02-16 05:40:30 +0000

    Import of FRODWITH/POE-Component-Server-PSGI-0.5 from CPAN.
    gitpan-cpan-distribution: POE-Component-Server-PSGI
    gitpan-cpan-version:      0.5
    gitpan-cpan-path:         FRODWITH/POE-Component-Server-PSGI-0.5.tar.gz
    gitpan-cpan-author:       FRODWITH
    gitpan-cpan-maturity:     released

---
+++ lib/Plack/Server/POE.pm
@@ -0,0 +1,20 @@
+package Plack::Server::POE;
+our $VERSION = '0.5';
+use base 'POE::Component::Server::PSGI';
+
+=head1 NAME
+
+Plack::Server::POE
+
+=head1 VERSION
+
+version 0.5
+
+=head1 DESCRIPTION
+
+This name is DEPRECATED.  The module is called POE::Component::Server::PSGI
+now.
+
+=cut
+
+1;
